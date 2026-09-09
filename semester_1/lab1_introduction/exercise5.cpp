bcdff197ebd48c79046bf4c080990aed937f66e0
Roma <romka.tretyak@gmail.com>
2026-09-09 11:13:49 +0300

    fix

--- semester_1/lab1_introduction/exercise5.cpp
+++ semester_1/lab1_introduction/exercise5.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 
 int main(){
-    int n;
-    int f = 0, s = 0,next;
+    int n, next;
+    int f = 0;
+    int s = 1;
     std::cout << "сколько чисел фибоначи вы хотите вывести\n";
     std::cin >> n;
-    for (int i = 0 ; i <=n ; i++){
-        if( n == f){
-            std::cout<< f; 
-        }else
-        if (n == s){
-            std::cout<< s; 
-        }
+    for (int i = 0 ; i <n ; i++){
+        std::cout << f << " ";
         next = f+s;
         f = s;
         s = next;
-        std::cout << next;
     }
     return 0;
 }
