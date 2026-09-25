f5645469e4a5a29580a5dda9ec7db618baf8a1aa
Roma <romka.tretyak@gmail.com>
2026-09-25 12:21:16 +0300

    fix

--- semester_1/lab2_integer_arithmetics/variant9.cpp
+++ semester_1/lab2_integer_arithmetics/variant9.cpp
@@ -7,12 +7,13 @@ int main(){
     if (!(std::cin >> a >> b)){
         
         std::cout << "Введите пожалуйста число, а не слово " << std::endl;
-    }else
+        return -1;
+    }
 
     if (a <= 0 || b <= 0 || b <= a) {
         std::cout << "a и b должны быть больше нуля . a должно быть меньше b" << std::endl;
-        
-    }else
+        return -1;
+    }
 
     for( int i = a; i <= b ; i++ ){
         orig = i;
@@ -27,8 +28,9 @@ int main(){
 
         if ( orig == reverse){
             std::cout << orig << " ";
-        }else;
+        }
     }
+    
 
     return 0;
 }
